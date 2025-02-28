35fcfbb1fe5fea9aff59cc143402c0ede6827842
palkanis <palkanis@msu.edu>
2025-02-28 18:45:06 +0530

    Create playGame function
    Use a while loop to iterate 5 times
    
    Use exact function calls to reset after every round
    
    Need to work on resetting function calls

--- javascript.js
+++ javascript.js
@@ -33,46 +33,72 @@ function getHumanChoice() {
     }
 }
 
-let humanScore = 0
 
-let computerScore = 0
 
-function playRound(humanChoice, computerChoice) {
+function playGame() {
 
-    if ((humanChoice === 'rock' && computerChoice === 'Rock') || (humanChoice === 'paper' && computerChoice === 'Paper') || (humanChoice === 'scissors' && computerChoice === 'Scissors')) {
-        return "It's a tie! Play another round.";
-    }
-    else if (humanChoice === 'rock' && (computerChoice === 'Scissors' || computerChoice === 'Paper')) {
-        if (computerChoice === 'Scissors') {
-            humanScore += 1;
-            return 'Human wins! Rock beats Scissors';
+    let humanScore = 0
+
+    let computerScore = 0
+
+    function playRound(humanChoice, computerChoice) {
+
+        if ((humanChoice === 'rock' && computerChoice === 'Rock') || (humanChoice === 'paper' && computerChoice === 'Paper') || (humanChoice === 'scissors' && computerChoice === 'Scissors')) {
+            return "It's a tie! Play another round.";
+        }
+        else if (humanChoice === 'rock' && (computerChoice === 'Scissors' || computerChoice === 'Paper')) {
+            if (computerChoice === 'Scissors') {
+                humanScore += 1;
+                return 'Human wins! Rock beats Scissors';
+            }
+            else {
+                computerScore += 1;
+                return 'Computer wins! Paper beats Rock';
+            }
+        }
+        else if (humanChoice === 'paper' && (computerChoice === 'Scissors' || computerChoice === 'Rock')) {
+            if (computerChoice === 'Scissors') {
+                computerScore += 1;
+                return 'Computer wins! Scissors beats Paper';
+            }
+            else {
+                humanScore += 1;
+                return 'Human wins! Paper beats Rock';
+            }
+        }
+        else if (humanChoice === 'scissors' && (computerChoice === 'Paper' || computerChoice === 'Rock')) {
+            if (computerChoice === 'Paper') {
+                humanScore += 1;
+                return 'Human wins! Scissors beats Paper';
+            }
+            else {
+                computerScore += 1;
+                return 'Computer wins! Rock beats Scissors';  
+            }
         }
         else {
-            computerScore += 1;
-            return 'Computer wins! Paper beats Rock';
+            return 'Not a valid choice!';
         }
+
     }
-    else if (humanChoice === 'paper' && (computerChoice === 'Scissors' || computerChoice === 'Rock')) {
-        if (computerChoice === 'Scissors') {
-            computerScore += 1;
-            return 'Computer wins! Scissors beats Paper';
-        }
-        else {
-            humanScore += 1;
-            return 'Human wins! Paper beats Rock';
-        }
+
+    i = 0
+    while (i < 5) {
+        i++;
+        console.log(playRound(getHumanChoice(),getComputerChoice()))
+        console.log('Human:', humanScore, 'Computer:', computerScore)
     }
-    else if (humanChoice === 'scissors' && (computerChoice === 'Paper' || computerChoice === 'Rock')) {
-        if (computerChoice === 'Paper') {
-            humanScore += 1;
-            return 'Human wins! Scissors beats Paper'
-        }
-        else {
-            computerScore += 1;
-            return 'Computer wins! Rock beats Scissors'  
-        }
-    }     
+
+
+    if (humanScore > computerScore) {
+        return 'Human Wins'
+    }
+    else if (computerScore > humanScore) {
+        return 'Computer wins'
+    }
+    else {
+        return 'The game is tied'
+    } 
 }
-console.log(playRound(getHumanChoice(), getComputerChoice()))
-console.log('Human:', humanScore)
-console.log('Computer:', computerScore)
+
+console.log(playGame())
